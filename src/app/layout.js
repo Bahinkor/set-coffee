27bdad2238b79feedeba4e18392687071c59d522
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-09-16 21:03:52 +0330

    index metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,16 @@
-import './globals.css'
+import './globals.css';
+
+// meta data tags foe seo
+export const metadata = {
+    title: "فروشگاه اینتزنتی ست کافی | صفحه اصلی | Set Coffee",
+    description: "test description",
+    icons: {
+        icon: "/images/favicon.svg",
+    }
+}
 
 export default function RootLayout({children}) {
+    // jsx
     return (
         <html lang="fa">
         <body>{children}</body>
