32a0b1955c3dde209352187b5df43781f552e418
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-09-17 13:57:10 +0330

    Use Image NextJS component

--- src/components/modules/product/Product.js
+++ src/components/modules/product/Product.js
@@ -1,46 +1,48 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./product.module.css";
-import { FaRegStar, FaStar } from "react-icons/fa";
-import { CiSearch, CiHeart } from "react-icons/ci";
+import {FaRegStar, FaStar} from "react-icons/fa";
+import {CiSearch, CiHeart} from "react-icons/ci";
 
 const Card = () => {
-  return (
-    <div className={styles.card}>
-      <div className={styles.details_container}>
-        <img
-          src="https://set-coffee.com/wp-content/uploads/2021/10/041-430x430.png"
-          alt=""
-        />
-        <div className={styles.icons}>
-          <Link href="/">
-            <CiSearch />
-            <p className={styles.tooltip}>مشاهده سریع</p>
-          </Link>
-          <div>
-            <CiHeart />
-            <p className={styles.tooltip}>افزودن به علاقه مندی ها </p>
-          </div>
-        </div>
-        <button>افزودن به سبد خرید</button>
-      </div>
+    return (
+        <div className={styles.card}>
+            <div className={styles.details_container}>
+                <Image
+                    src="/images/product-image.png"
+                    alt="Coffee Product Image"
+                    width="280"
+                    height="300"
+                />
+                <div className={styles.icons}>
+                    <Link href="/">
+                        <CiSearch/>
+                        <p className={styles.tooltip}>مشاهده سریع</p>
+                    </Link>
+                    <div>
+                        <CiHeart/>
+                        <p className={styles.tooltip}>افزودن به علاقه مندی ها </p>
+                    </div>
+                </div>
+                <button>افزودن به سبد خرید</button>
+            </div>
 
-      <div className={styles.details}>
-        <Link href={"/"}>
-          کپسول قهوه SETpresso سازگار با دستگاه نسپرسو ( RED ) 10 عددی LIMITED
-          EDITION
-        </Link>
-        <div>
-          <FaStar />
-          <FaStar />
-          <FaStar />
-          <FaRegStar />
-          <FaRegStar />
+            <div className={styles.details}>
+                <Link href={"/"}>
+                    کپسول قهوه SETpresso سازگار با دستگاه نسپرسو ( RED ) 10 عددی LIMITED
+                    EDITION
+                </Link>
+                <div>
+                    <FaStar/>
+                    <FaStar/>
+                    <FaStar/>
+                    <FaRegStar/>
+                    <FaRegStar/>
+                </div>
+                <span>825,000 تومان</span>
+            </div>
         </div>
-        <span>825,000 تومان</span>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Card;
