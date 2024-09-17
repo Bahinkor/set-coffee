c9d0ea9e28898e2eb9df3ca0077025cf512bc0c6
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-09-17 17:40:54 +0330

    Developing login and signup page

---
+++ src/app/login-register/page.js
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import styles from "@/styles/login-register.module.css";
+import {useState} from "react";
+import {authTypes} from "@/utils/constants";
+
+import Login from "@/components/templates/login-register/Login";
+import Register from "@/components/templates/login-register/Register";
+
+const LoginRegister = () => {
+    const [authType, setAuthType] = useState(authTypes.LOGIN);
+
+    const showRegisterForm = () => setAuthType(authTypes.REGISTER);
+    const showLoginForm = () => setAuthType(authTypes.LOGIN);
+
+    return (
+        <div className={styles.login_register}>
+            <div className={styles.form_bg} data-aos="fade-up">
+                {authType === authTypes.LOGIN ? (
+                    <Login showRegisterForm={showRegisterForm}/>
+                ) : (
+                    <Register showLoginForm={showLoginForm}/>
+                )}
+            </div>
+            <section>
+                <Image
+                    src="/images/login-form-image.webp"
+                    alt="Cofee Background Image"
+                    width="1200"
+                    height="800"
+                />
+            </section>
+        </div>
+    );
+};
+
+export default LoginRegister;
