b27cc565dbe87c409c68755558a06537ff095c86
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-09-17 18:46:31 +0330

    Development Register Form

--- src/components/templates/login-register/Register.js
+++ src/components/templates/login-register/Register.js
@@ -1,38 +1,61 @@
+"use client"
+
+import React, {useState} from "react";
+import Link from "next/link";
+import Sms from "@/components/templates/login-register/Sms";
 import styles from "./register.module.css";
 
 const Register = ({showLoginForm}) => {
+    // state
+    const [isRegisterWithPassword, setIsRegisterWithPassword] = useState(false);
+    const [isRegisterWithOtp, setIsRegisterWithOtp] = useState(false);
+
+    // function
+    const hideOtpFrom = () => setIsRegisterWithOtp(false);
+
+    // jsx
     return (
         <>
-            <div className={styles.form}>
-                <input className={styles.input} type="text" placeholder="نام"/>
-                <input
-                    className={styles.input}
-                    type="text"
-                    placeholder="شماره موبایل  "
-                />
-                <input
-                    className={styles.input}
-                    type="email"
-                    placeholder="ایمیل (دلخواه)"
-                />
-                {/*{registerWithPass && (*/}
-                    <input
-                        className={styles.input}
-                        type="password"
-                        placeholder="رمز عبور"
-                    />
-                {/*)}*/}
-                <p style={{marginTop: "1rem"}} className={styles.btn}>
-                    ثبت نام با کد تایید
-                </p>
-                <button style={{marginTop: ".7rem"}} className={styles.btn}>
-                    ثبت نام با رمزعبور
-                </button>
-                <p className={styles.back_to_login} onClick={showLoginForm}>برگشت به ورود</p>
-            </div>
-            <p className={styles.redirect_to_home}>لغو</p>
-
-            {/* <Sms /> */}
+            {
+                !isRegisterWithOtp ? (
+                    <>
+                        <div className={styles.form}>
+                            <input className={styles.input} type="text" placeholder="نام"/>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                placeholder="شماره موبایل  "
+                            />
+                            <input
+                                className={styles.input}
+                                type="email"
+                                placeholder="ایمیل (دلخواه)"
+                            />
+                            {
+                                isRegisterWithPassword && (
+                                    <input
+                                        className={styles.input}
+                                        type="password"
+                                        placeholder="رمز عبور"
+                                    />
+                                )
+                            }
+                            <p style={{marginTop: "1rem"}} className={styles.btn}
+                               onClick={() => setIsRegisterWithOtp(true)}>
+                                ثبت نام با کد تایید
+                            </p>
+                            <button style={{marginTop: ".7rem"}} className={styles.btn}
+                                    onClick={() => setIsRegisterWithPassword(true)}>
+                                ثبت نام با رمزعبور
+                            </button>
+                            <p className={styles.back_to_login} onClick={showLoginForm}>برگشت به ورود</p>
+                        </div>
+                        <Link href="/" className={styles.redirect_to_home}>لغو</Link>
+                    </>
+                ) : (
+                    <Sms hideOtp={hideOtpFrom}/>
+                )
+            }
         </>
     );
 };
