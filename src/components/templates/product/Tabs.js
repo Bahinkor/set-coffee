72056e2199ff7308228d4664068d083211f0d03c
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-09-21 22:51:59 +0330

    Optimization...

--- src/components/templates/product/Tabs.js
+++ src/components/templates/product/Tabs.js
@@ -1,69 +1,62 @@
 "use client";
 import React from "react";
 import styles from "./tabs.module.css";
-import { useState } from "react";
+import {useState} from "react";
 import Description from "./Description";
 import MoreInfoes from "./MoreInfoes";
 import Comments from "./Comments";
-const Tabs = ({productData}) => {
-  const [tab, setTab] = useState("description");
-  return (
-    <div data-aos="fade-left" className={styles.tabs}>
-      <input
-        onClick={() => setTab("description")}
-        type="radio"
-        id="description"
-        name="tab-control"
-        checked={tab == "description" && "checked"}
-      />
-      <input
-        onClick={() => setTab("moreInfoes")}
-        type="radio"
-        id="moreInfoes"
-        name="tab-control"
-        checked={tab == "moreInfoes" && "checked"}
-      />
-      <input
-        onClick={() => setTab("comments")}
-        type="radio"
-        id="comments"
-        name="tab-control"
-        checked={tab == "comments" && "checked"}
-      />
-      <ul>
-        <li title="Features">
-          <label htmlFor="description" role="button">
-            {" "}
-            توضیحات{" "}
-          </label>
-        </li>
-        <li title="Delivery Contents">
-          <label htmlFor="moreInfoes" role="button">
-            {" "}
-            اطلاعات بیشتر{" "}
-          </label>
-        </li>
-        <li title="Shipping">
-          <label htmlFor="comments" role="button">
-            {" "}
-            نظرات ({productData.comments.filter((comment) => comment.isAccept).length}){" "}
-          </label>
-        </li>
-      </ul>
 
-      <div className={styles.contents}>
-        <section className={styles.tabs_content}>
-          <Description />
-        </section>
-        <section className={styles.tabs_content}>
-          <MoreInfoes productData={JSON.parse(JSON.stringify(productData))} />
-        </section>
-        <section className={styles.tabs_content}>
-          <Comments productID={productData._id} comments={productData.comments} />
-        </section>
-      </div>
-    </div>
-  );
+const Tabs = ({productData}) => {
+    const [tab, setTab] = useState("description");
+
+    return (
+        <>
+            <div data-aos="fade-left" className={styles.tabs}>
+                <ul>
+                    <li>
+                        <button
+                            className={tab === "description" ? styles.active_tab : ""}
+                            onClick={() => setTab("description")}
+                        >
+                            توضیحات
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            className={tab === "moreInfoes" ? styles.active_tab : ""}
+                            onClick={() => setTab("moreInfoes")}
+                        >
+                            اطلاعات بیشتر
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            className={tab === "comments" ? styles.active_tab : ""}
+                            onClick={() => setTab("comments")}
+                        >
+                            نظرات (
+                            {productData.comments.filter((comment) => comment.isAccept).length})
+                        </button>
+                    </li>
+                </ul>
+
+                <div className={styles.contents}>
+                    <section>
+                        {tab === "description" && <Description/>}
+                        {tab == "moreInfoes" && (
+                            <MoreInfoes productData={JSON.parse(JSON.stringify(productData))}/>
+                        )}
+                        {tab == "comments" && (
+                            <Comments
+                                productID={productData._id}
+                                comments={JSON.parse(JSON.stringify(productData.comments))}
+                            />
+                        )}
+                    </section>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Tabs;
